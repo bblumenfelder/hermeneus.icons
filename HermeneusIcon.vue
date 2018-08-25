34a84e00e858ff259b07cacd7a6dc15fe69b0c8c
bblumenfelder <benedikt.blumenfelder@gmail.com>
2018-08-25 16:14:59 +0200

    vue-component

---
+++ HermeneusIcon.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <img :src="icon" :width="CalcWidth" :height="CalcHeight" alt="">
+    </div>
+</template>
+
+<script>
+    let IconPathString = {
+        'success': '/img/svg/iris-icon-success.svg',
+        'danger': '/img/svg/iris-icon-danger.svg',
+        'info': '/img/svg/iris-icon-info.svg',
+        'warning': '/img/svg/iris-icon-danger.svg',
+        'confirm': '/img/svg/iris-icon-confirm.svg',
+    };
+
+    export default {
+        name: "hermeneus-icon",
+        props: ['name', 'width', 'height'],
+        computed: {
+            CalcWidth: function () {
+                return this.width ? this.width : '15';
+            },
+            CalcHeight: function () {
+                return this.height ? this.height : '15';
+            },
+            icon: function () {
+                return IconPathString[this.name]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
